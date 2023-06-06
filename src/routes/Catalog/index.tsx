b725ae1695384fbac980fc68bcc6d6da5737a74a
Samuk <samukcarlos@gmail.com>
2023-06-06 16:12:50 -0300

    Catalog e cards

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import Header from "../../components/Header";
 import SearchBar from '../../components/SearchBar';
+import Card from '../../components/Card';
 
 export default function Catalog(){
     return (
@@ -15,6 +16,16 @@ export default function Catalog(){
                 </div>
 
                 <div className="ct-catalog-cards-container">
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
+                  <Card />
 
                 </div>
 
