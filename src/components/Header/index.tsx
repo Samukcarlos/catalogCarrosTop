70ce85de693e0b31e545286560760e1502ad095b
Samuk <samukcarlos@gmail.com>
2023-06-06 15:34:32 -0300

    Search bar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import './styles.css';
 export default function Header(){
    return (
             <head>
-                <div className="ctçontainer">
+                <div className="ct-container">
                 <h1>Carros Top</h1>
                 </div>               
             </head>
